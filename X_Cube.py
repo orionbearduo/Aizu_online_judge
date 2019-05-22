ffb286b1d49ba9d7f0a97a2267fe41f43c627c96
Orion <orionbearduo@gmail.com>
2019-05-22 11:35:26 +0900

    Create X_Cube.py

---
+++ X_Cube.py
@@ -0,0 +1,19 @@
+"""
+Write a program which calculates the cube of a given integer x.
+*Input
+An integer x is given in a line.
+*Output
+Print the cube of x in a line.
+
+Sample Input 1
+2
+Sample Output 1
+8
+Sample Input 2
+3
+Sample Output 2
+27
+"""
+
+# Solution
+print(int(input())**3)
