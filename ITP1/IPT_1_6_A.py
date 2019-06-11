e27016d1f50538da81ae6e6c931b14e5d08552f4
Orion <orionbearduo@gmail.com>
2019-06-11 22:37:49 +0900

    Create IPT_1_6_A.py

---
+++ ITP1/IPT_1_6_A.py
@@ -0,0 +1,15 @@
+"""
+Reversing Numbers
+
+Write a program which reads a sequence and prints it in the reverse order.
+
+Input
+The input is given in the following format:
+
+n
+a1 a2 . . . an
+n is the size of the sequence and ai is the ith element of the sequence.
+
+Output
+Print the reversed sequence in a line. Print a single space character between adjacent elements (Note that your program should not put a space character after the last element).
+"""
