d3d50607bc6c46e011237c01ee34ebde2a619f9a
orionbearduo <orionbearduo@gmail.com>
2019-06-14 00:11:42 +0900

    update

--- ITP1/IPT_1_6_A.py
+++ ITP1/IPT_1_6_A.py
@@ -13,3 +13,10 @@ n is the size of the sequence and ai is the ith element of the sequence.
 Output
 Print the reversed sequence in a line. Print a single space character between adjacent elements (Note that your program should not put a space character after the last element).
 """
+n = int(input())
+
+table = list(map(int, input().split()))
+
+table.reverse()
+
+print(' '.join(map(str, table)))
