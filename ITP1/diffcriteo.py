03b55890da4797a5c7299f6e3895deb581633f9d
icko <icko@yahoo-corp.jp>
2019-08-26 12:54:41 +0900

    yjdspv2

--- ITP1/diffcriteo.py
+++ ITP1/diffcriteo.py
@@ -24,8 +24,8 @@ if __name__ == '__main__':
                 result[adprodset_code.strip('\n')] = {'params': params}
         return result
 
-    before_params = dividing('/Users/icko/Documents/000/criteo/stg/adprodsetbetabefore8161645')
-    after_params = dividing('/Users/icko/Documents/000/criteo/stg/adprodsetbetaafter8161645')
+    before_params = dividing('/Users/icko/Documents/000/criteo/stg/adprodsetprodbefore8201450')
+    after_params = dividing('/Users/icko/Documents/000/criteo/stg/adprodsetprodafter8201450')
 
     for k in set(before_params.keys()) - set(after_params.keys()):
         print('[Caution] ' + k + ' vanished!')
@@ -36,35 +36,4 @@ if __name__ == '__main__':
     for v in before_params.keys():
         before_obj = before_params[v]['params']
         after_obj = after_params[v]['params']
-        if before_obj['height'] == after_obj['height'] and \
-           before_obj['width'] == after_obj['width'] and before_obj['callback'] == after_obj['callback'] and \
-           before_obj['creative_options'] == after_obj['creative_options'] and before_obj['js_file'] == after_obj['js_file'] \
-                and before_obj['zone_id'] == after_obj['zone_id']:
-            print('Product [Criteo] is no problem')
-        else:
-            if 'api_timeout' not in before_obj.keys():
-                print(v + ' api_timeout [key] is not exist\n')
-                continue
-            if before_obj['callback'] != after_obj['callback']:
-                print('Callback in adprodset_code ' + v + ' is different')
-            else:
-                print(v + ' Position is OK')
-            if before_obj['width'] != after_obj['width']:
-                print('Width in adprodset_code ' + v + ' is different')
-            else:
-                print(v + ' Width is OK')
-
-            if before_obj['height'] != after_obj['height']:
-                print('Width in adprodset_code ' + v + ' is different')
-            else:
-                print(v + ' Height is OK')
-            if before_obj['creative_options'] != after_obj['creative_options']:
-                print('creative_options in adprodset_code ' + v + ' is different')
-            else:
-                print(v + ' creative_options is OK')
-            if before_obj['zone_id'] != after_obj['zone_id']:
-                print('zone_id in adprodset_code ' + v + ' is different')
-            else:
-                print(v + ' zone_id is OK')
-
-
+        print(v.count())
